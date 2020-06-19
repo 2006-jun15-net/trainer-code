8eb0223ed7d957864fb0bcf940956ba7aff76245
Nick Escalona <nicholas.escalona@gmail.com>
2020-06-19 10:56:40 -0500

    async, disposable, using

--- 1-csharp/Serialization/Serialization/Program.cs
+++ 1-csharp/Serialization/Serialization/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Serialization.DataModel;
 
@@ -8,7 +9,7 @@ namespace Serialization
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             // (for your program, the starting directory is bin/Debug/netcoreapp3.1)
             string filePath = "../../../data.json";
@@ -16,7 +17,7 @@ namespace Serialization
             List<PlayerStats> data;
             try
             {
-                string initialJson = File.ReadAllText(filePath);
+                string initialJson = await File.ReadAllTextAsync(filePath);
                 data = JsonConvert.DeserializeObject<List<PlayerStats>>(initialJson);
             }
             catch (FileNotFoundException)
@@ -28,12 +29,61 @@ namespace Serialization
 
             string json = ConvertToJson(data);
 
-            WriteStringToFile(filePath, json);
+            await WriteStringToFileAsync(filePath, json);
+
+            // at this point,the file is definitely written
         }
 
-        public static void WriteStringToFile(string filePath, string json)
+        // when doing something async:
+        // 1. call the Async version of whatever library method.
+        // 2. await that Task.
+        // 3. mark the current method with the async modifier.
+        // 4. if the method returns type T, change it to return type Task<T>
+        //    if it returns void, change it to return type Task
+        // 5. as a matter of convention, add the Async suffix to your method name.
+        // (continue again from step 1 on the parts of your code that are now broken)
+        public async static Task WriteStringToFileAsync(string filePath, string json)
         {
-            File.WriteAllText(filePath, json);
+            //await File.WriteAllTextAsync(filePath, json);
+            // for more control over the file I/O, we would usually open a FileStream object.
+
+            // the CLR manages the memory for all the CLR objects with garbage collection.
+            //    (otherwise, there would be memory leaks any time i failed to manually clean up any object.)
+
+            // any time you have .NET code open or access some resource OUTSIDE the CLR
+            //  (like the hard drive), you do need to manually tell it when you are done to avoid problems.
+            // the IDisposable interface is implemented by any class which you need to do this for.
+
+            //FileStream fileStream = null;
+            try
+            {
+                // if you are fine with the resource not being disposed until the variable goes out of scope...
+                // you can use this form of the using statement.
+                using var fileStream = new FileStream(filePath, FileMode.Create);
+
+                //fileStream. // pretend i finished the code
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Error writing file: {ex.Message}");
+            }
+            //finally
+            //{
+            //    // what if an exception is thrown at any point beforehand?
+            //    //if (fileStream != null)
+            //    //{
+            //    //    fileStream.Dispose();
+            //    //}
+            //    fileStream?.Dispose();
+            //    // C# has an operator "?." which calls a method (or accesses a property/field/etc.)
+            //    // but only if the thing to the left is not null.
+            //    // the finally block always runs whether there was no exception, a caught exception, or an uncaught exception.
+            //}
+            // C# has a "using statement" to replace the boilerplate "resource = null, try, finally, resource.Dispose"
+            //using (var fileStream = new FileStream(filePath, FileMode.Create))
+            //{
+
+            //} // right here, at the closing brace, is effectively "finally { resource?.Dispose() }"
         }
 
         /// <summary>
