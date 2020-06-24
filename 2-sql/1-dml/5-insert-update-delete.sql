470ae718c9ed4f50f00080f359e9d3fdc4605258
Nick Escalona <nicholas.escalona@gmail.com>
2020-06-24 14:09:41 -0500

    dml

---
+++ 2-sql/1-dml/5-insert-update-delete.sql
@@ -0,0 +1,38 @@
+-- rest of DML besides SELECT is for adding/changing/removing rows
+
+-- INSERT
+
+SELECT * FROM Genre;
+
+INSERT INTO Genre VALUES (100, 'Miscellaneous');
+
+INSERT INTO Genre (GenreId) VALUES (101);
+
+INSERT INTO Genre (GenreId, Name) VALUES (102, 'Misc 2');
+
+SELECT * FROM Genre WHERE GenreId >= 100;
+
+INSERT INTO Genre (GenreId, Name) VALUES
+	(103, 'Misc 3'),
+	(104, 'Misc 4');
+
+INSERT INTO Genre (GenreId, Name)
+	SELECT GenreId + 10, Name + '!'
+	FROM Genre
+	WHERE GenreId = 104;
+
+-- INSERT can also do things like read CSV files etc
+
+-- UPDATE
+
+-- without a WHERE, would modify every row
+UPDATE Genre
+SET Name = 'Misc 1'  -- , otherthing = value
+WHERE GenreId = 101;
+
+-- without a WHERE, would delete every row (one by one)
+DELETE FROM Genre
+WHERE GenreId >= 100;
+
+-- this command deletes all rows all at once
+--TRUNCATE TABLE Genre;
