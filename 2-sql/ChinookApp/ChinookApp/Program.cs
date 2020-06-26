71e5192c8eb47866bd31ace4f76977ec1aa37655
Nick Escalona <nicholas.escalona@gmail.com>
2020-06-26 10:02:48 -0500

    some ef work

--- 2-sql/ChinookApp/ChinookApp/Program.cs
+++ 2-sql/ChinookApp/ChinookApp/Program.cs
@@ -63,35 +63,39 @@ namespace ChinookApp
         {
             DisplayData();
 
-            Console.WriteLine("\nAdding some data...");
-            AddSomeDataFromUserInput();
+            //Console.WriteLine("\nAdding some data...");
+            //AddSomeDataFromUserInput();
 
-            DisplayData();
+            //DisplayData();
 
-            Console.WriteLine("\nUpdating some data...");
-            UpdateSomeData();
+            //Console.WriteLine("\nUpdating some data...");
+            //UpdateSomeData();
 
-            DisplayData();
+            //DisplayData();
 
-            Console.WriteLine("\nDeleting some data...");
-            DeleteSomeData();
+            //Console.WriteLine("\nDeleting some data...");
+            //DeleteSomeData();
 
-            DisplayData();
+            //DisplayData();
         }
 
         public static void DeleteSomeData()
         {
-            throw new NotImplementedException();
+            // there's actually no way to delete in EF without first fetching the object.
+            // first, get the thing, then, remove it from its DbSet, then SaveChanges
         }
 
         public static void UpdateSomeData()
         {
-            throw new NotImplementedException();
+            // every object that you pull from the context is "tracked" by the context
+            // when you call SaveChanges, the context will send all changes that have been
+            // noticed automatically on the tracked entities
         }
 
         public static void AddSomeDataFromUserInput()
         {
-            throw new NotImplementedException();
+            // for adding, you also don't need to worry about foreign key values.
+            // you can add/change relationships between objects via the navigation
         }
 
         public static void DisplayData()
@@ -103,13 +107,56 @@ namespace ChinookApp
             //    .Where(e => e.Title.Contains("sales")) // this is now running in .NET, not in SQL
             //    .ToList();
 
-            List<Employee> salespeople = context.Employee
-                .Where(e => e.Title.Contains("sales")) // this will be translated to SQL
-                .ToList(); // it generates the SQL and fetches the objects
+            //List<Employee> salespeople = context.Employee
+            //    .Where(e => e.Title.Contains("sales")) // this will be translated to SQL
+            //    .ToList(); // it generates the SQL and fetches the objects
 
-            foreach (Employee person in salespeople)
+            //foreach (Employee person in salespeople)
+            //{
+            //    Console.WriteLine($"{person.Title} {person.FirstName} {person.LastName}");
+            //}
+
+            // EF by default only loads the data from one table at a time.
+            //    therefore, the navigation properties will be null or empty.
+            // if you need those properties to be filled in, you have to tell EF somehow.
+            
+            // there are three ways to tell EF to fill them in:
+            //  1. eager loading (do this one): call Include (and maybe ThenInclude) methods
+            //        (telling EF in the query itself to join with other tables)
+            //  2. lazy loading (avoid this one): can be enabled in the dbcontext options...
+            //       it will load each navigation property in the moment you access it.
+            //       for very simple cases, minimal convenience
+            //       for anything more complicated, the performance impact is too much
+            //          (N+1 problem)
+            //  3. explicit loading (rarely needed): given an actual object taken from the context
+            //        we can tell EF to fill in individual properties
+
+            // good practice with entity framework:
+            // 1. pay attention to when the query is actually sent to the DB (e.g. ToList())
+            // 2. try to get all the data you need at a given moment in one query rather than several.
+            // 3. use eager loading (Include) rather than lazy or explicit.
+            // 4. avoid using foreign key values when the navigation properties work instead.
+
+            List<Student> students = context.Student
+                .Include(s => s.Enrollment)
+                    .ThenInclude(e => e.Course)
+                .ToList();
+
+            foreach (var student in students)
             {
-                Console.WriteLine($"{person.Title} {person.FirstName} {person.LastName}");
+                // bad code - inefficiently coding a join by hand in C#
+                // the whole point of ORM is avoiding this kind of code
+                //var courseIds = context.Enrollment
+                //    .Where(e => e.StudentId == student.Id)
+                //    .Select(e => e.CourseId)
+                //    .ToList();
+                //var courses = context.Course
+                //    .Where(c => courseIds.Contains(c.Id))
+                //    .Select(c => c.CourseNumber);
+                var courses = student.Enrollment.Select(e => e.Course.CourseNumber);
+
+                var coursesString = string.Join(", ", courses);
+                Console.WriteLine($"[{student.Id}] {student.Name}: {coursesString}");
             }
         }
     }
