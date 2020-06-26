74c2ea0a8bb582cbbdc1a5d99116470c65bb483b
Nick Escalona <nicholas.escalona@gmail.com>
2020-06-26 11:49:08 -0500

    finish ef demo

--- 2-sql/ChinookApp/ChinookApp/Program.cs
+++ 2-sql/ChinookApp/ChinookApp/Program.cs
@@ -61,28 +61,67 @@ namespace ChinookApp
 
         static void Main(string[] args)
         {
+            DoSomethingWithInvoices();
             DisplayData();
 
-            //Console.WriteLine("\nAdding some data...");
-            //AddSomeDataFromUserInput();
+            Console.WriteLine("\nAdding some data...");
+            AddSomeDataFromUserInput();
 
-            //DisplayData();
+            DisplayData();
 
-            //Console.WriteLine("\nUpdating some data...");
-            //UpdateSomeData();
+            Console.WriteLine("\nUpdating some data...");
+            UpdateSomeData();
 
-            //DisplayData();
+            DisplayData();
 
-            //Console.WriteLine("\nDeleting some data...");
-            //DeleteSomeData();
+            Console.WriteLine("\nDeleting some data...");
+            DeleteSomeData();
 
-            //DisplayData();
+            DisplayData();
         }
 
         public static void DeleteSomeData()
         {
             // there's actually no way to delete in EF without first fetching the object.
             // first, get the thing, then, remove it from its DbSet, then SaveChanges
+            using var context = new ChinookContext(Options);
+
+            // i'll delete all the students in the physics course, and the physics course too.
+            var course = context.Course
+                .Include(c => c.Enrollment)
+                    .ThenInclude(e => e.Student)
+                .FirstOrDefault(c => c.Id == 1000);
+
+            context.Course.Remove(course); // this will result in removing the enrollments too,
+                                           // instead of an error, because the FK constraint has ON DELETE CASCADE
+
+            context.Student.RemoveRange(course.Enrollment.Select(e => e.Student));
+            // remove every student reachable from that course
+
+            context.SaveChanges(); // no changes go the DB until here
+        }
+
+        // get the first customer the database has to give, print all tracks purchased.
+        public static void DoSomethingWithInvoices()
+        {
+            using var context = new ChinookContext(Options);
+
+            var customer = context.Customer
+                .Include(c => c.Invoice)
+                    .ThenInclude(i => i.InvoiceLine)
+                        .ThenInclude(il => il.Track)
+                .FirstOrDefault();
+
+            // SelectMany works like Select, except you can "select" a whole collection of stuff for each element
+            //    and it will flatten that out to one big list.
+
+            IEnumerable<Track> tracks = customer.Invoice
+                .SelectMany(i => i.InvoiceLine.Select(il => il.Track));
+
+            foreach (var track in tracks)
+            {
+                Console.WriteLine(track.Name);
+            }
         }
 
         public static void UpdateSomeData()
@@ -90,12 +129,64 @@ namespace ChinookApp
             // every object that you pull from the context is "tracked" by the context
             // when you call SaveChanges, the context will send all changes that have been
             // noticed automatically on the tracked entities
+
+            using var context = new ChinookContext(Options);
+
+            var course = context.Course.Find(1000); // get the course with PK 1000 (hardcoded in Add method below)
+
+            course.CourseNumber = "PHYS102"; // since this entity is tracked, the context will pick up on that change.
+
+            // if it weren't tracked, i would call context.Course.Update(course);
+            context.SaveChanges();
         }
 
         public static void AddSomeDataFromUserInput()
         {
             // for adding, you also don't need to worry about foreign key values.
             // you can add/change relationships between objects via the navigation
+
+            Console.WriteLine("Enter a new student name: ");
+            var name = Console.ReadLine();
+
+            using var context = new ChinookContext(Options);
+
+            var student = new Student { Name = name };
+
+
+            // with LINQ, "OrDefault" forms of the methods return null instead of throwing exception
+            //   in case of no results.
+
+            // create course if doesn't exist
+            // when we look up a object by primary key specifically, we can use DbSet.Find
+            //    it wont fetch from the database again, if the object is already cached in the context.
+            //    there's no way to combine with .Include
+
+            var course = context.Course
+                .Include(c => c.Enrollment)
+                .FirstOrDefault(c => c.CourseNumber == "PHYS101");
+            //var course = context.Course.Find(1000);
+            if (course == null)
+            {
+                course = new Course { Id = 1000, CourseNumber = "PHYS101" };
+                context.Course.Add(course);
+            }
+
+            // add the student (and an enrollment indirectly via the course)
+            // ...you can add and update stuff via the navigation properties
+            course.Enrollment.Add(new Enrollment { Student = student });
+
+            // notice... i don't need to fill in the foreign key values
+            // i don't even need o fill in more than one of the two navigation properties
+            //   associated with the relationships
+
+            //context.Add(student);
+            //context.Student.Add(student);
+            //context.Update(student);
+            //context.Student.Update(student);
+
+            context.SaveChanges();
+            // after savechanges, any new or updated stuff implicit in what i coded before
+            //   is filled in by EF for you on those tracked objects.
         }
 
         public static void DisplayData()
