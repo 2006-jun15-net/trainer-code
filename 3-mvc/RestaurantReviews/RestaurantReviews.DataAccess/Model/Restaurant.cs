5170a55265ae31638bf797ce309b800b797f13d6
Nick Escalona <nicholas.escalona@gmail.com>
2020-07-06 11:24:46 -0500

    add restaurant reviews v3

---
+++ 3-mvc/RestaurantReviews/RestaurantReviews.DataAccess/Model/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RestaurantReviews.DataAccess.Model
+{
+    public partial class Restaurant
+    {
+        public Restaurant()
+        {
+            Review = new HashSet<Review>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Review> Review { get; set; }
+    }
+}
---
+++ 3-mvc/RestaurantReviews/RestaurantReviews.DataAccess/Model/RestaurantReviewsDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace RestaurantReviews.DataAccess.Model
+{
+    public partial class RestaurantReviewsDbContext : DbContext
+    {
+        public RestaurantReviewsDbContext()
+        {
+        }
+
+        public RestaurantReviewsDbContext(DbContextOptions<RestaurantReviewsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Restaurant> Restaurant { get; set; }
+        public virtual DbSet<Review> Review { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(128);
+            });
+
+            modelBuilder.Entity<Review>(entity =>
+            {
+                entity.HasIndex(e => e.RestaurantId);
+
+                entity.Property(e => e.ReviewerName)
+                    .IsRequired()
+                    .HasMaxLength(128);
+
+                entity.Property(e => e.Text)
+                    .IsRequired()
+                    .HasMaxLength(2048);
+
+                entity.HasOne(d => d.Restaurant)
+                    .WithMany(p => p.Review)
+                    .HasForeignKey(d => d.RestaurantId);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
